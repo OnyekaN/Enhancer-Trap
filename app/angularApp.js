55353390c527914589b75af41b7054b6472ee9dd
OnyekaN <ognnaemeka@gmail.com>
2016-09-24 16:47:25 -0400

    Initial

---
+++ app/angularApp.js
@@ -0,0 +1,13 @@
+"use strict";
+//import uiRouter from 'angular-ui-router'
+
+(
+	angular
+		.module('app', [
+			require('angular-ui-router')
+	])
+
+)
+
+//don't forget to use "Controller.$inject = ['factory&||service']" for
+//dependencies
---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+var path = require('path');
+
+module.exports = {
+	entry: './app/angularApp.js',
+	output: {
+			path: path.join(__dirname, 'app/assets/js'),
+			filename: 'bundle.js'
+	},
+	module: {
+		loaders: [
+			{	
+				test: path.join(__dirname, 'app'),
+				loader: 'babel-loader',
+				exclude: /node_modules/,
+				query: {
+					presets: ['es2015']
+				}
+			}
+		]
+	}
+};
